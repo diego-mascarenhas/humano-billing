e96bd0979ddf3d996adc397c01490aae32308548
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-10-03 16:55:09 +0200

    Move enterprises.payment_type_id FK into payment_types migration; deprecate separate FK migration

---
+++ database/migrations/2025_10_03_120000_add_fk_enterprises_payment_type_id_if_exists.php
@@ -0,0 +1,11 @@
+<?php
+
+// Deprecated: FK moved into create_payment_types_table migration
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    public function up() {}
+    public function down() {}
+};
+
