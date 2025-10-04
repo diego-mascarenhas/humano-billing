3974907b320e500bf0b53d5051e25809bf74efac
Diego Mascarenhas <diego.mascarenhas@icloud.com>
2025-10-04 16:22:51 +0200

    feat: move accounting module to billing package

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,14 @@ Route::middleware(['web', 'auth'])->group(function ()
             return redirect()->route('payments.index');
         })->name('payment.index');
     });
+
+    // Accounting routes
+    Route::get('/accounting', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'index'])->name('accounting.index');
+    Route::get('/accounting/invoice/{id}', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'showInvoice'])->name('accounting.invoice');
+    Route::get('/accounting/invoice/{id}/download', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'downloadInvoice'])->name('accounting.invoice.download');
+    Route::get('/accounting/customer/{id}', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'customerInvoices'])->name('accounting.customer');
+    Route::get('/accounting/download-quarter', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'downloadQuarterInvoices'])->name('accounting.download-quarter');
+    Route::get('/accounting/download-quarter-csv', [\Idoneo\HumanoBilling\Http\Controllers\AccountingController::class, 'downloadQuarterCsv'])->name('accounting.download-quarter-csv');
 });
 
 
